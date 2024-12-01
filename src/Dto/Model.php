a04ff48d1fca26198b8065836f7c6db04a11a6b0
Nico Hiort af Ornäs <first-name@warpspeed.solutions>
2024-12-01 19:54:28 +0200

    Fix lint issues

--- src/Dto/Model.php
+++ src/Dto/Model.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nicodemuz\PhpGetImgAi\Dto;
 
 use DateTimeImmutable;
--- src/GetImgAiClient.php
+++ src/GetImgAiClient.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nicodemuz\PhpGetImgAi;
 
 use Exception;
